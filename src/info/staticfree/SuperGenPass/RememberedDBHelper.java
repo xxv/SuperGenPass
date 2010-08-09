789f28543975e82f94c6646d335ba9a9d0492947
Steve Pomeroy <steve@staticfree.info>
2010-08-09 19:09:47 -0400

    Cleaned up cursor closing.
    Also: automatic source whitespace cleanup.

--- src/info/staticfree/SuperGenPass/RememberedDBHelper.java
+++ src/info/staticfree/SuperGenPass/RememberedDBHelper.java
@@ -1,5 +1,6 @@
 package info.staticfree.SuperGenPass;
 
+import android.app.Activity;
 import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
@@ -13,7 +14,7 @@ public class RememberedDBHelper extends SQLiteOpenHelper {
 	private final static String DB_DOMAINS_TABLE = "domains";
 	private final static int DB_VERSION = 2;
 	private final Context context;
-	
+
 	public RememberedDBHelper(Context context) {
 		super(context, DB_NAME, null, DB_VERSION);
 		this.context = context;
@@ -23,7 +24,7 @@ public class RememberedDBHelper extends SQLiteOpenHelper {
 	public void onCreate(SQLiteDatabase db) {
 		db.execSQL("CREATE TABLE '"+DB_DOMAINS_TABLE+
 				"' ('_id' INTEGER PRIMARY KEY, 'domain' VARCHAR(255))");
-		
+
 	}
 
 	@Override
@@ -31,64 +32,69 @@ public class RememberedDBHelper extends SQLiteOpenHelper {
 		db.execSQL("DROP TABLE IF EXISTS "+ DB_DOMAINS_TABLE);
 		onCreate(db);
 	}
-	
+
 	   /**
-     * Adds the given domain to the list of remembered domains. 
-     * 
+     * Adds the given domain to the list of remembered domains.
+     *
      * @param domain the filtered domain name
      */
     static void addRememberedDomain(SQLiteDatabase db, String domain){
-    	final Cursor existingEntries = db.query(DB_DOMAINS_TABLE, null, 
-    			"domain=?", new String[] {domain}, 
+    	final Cursor existingEntries = db.query(DB_DOMAINS_TABLE, null,
+    			"domain=?", new String[] {domain},
     			null, null, null);
-    	
+
     	if (existingEntries.getCount() == 0) {
 	    	final ContentValues cv = new ContentValues();
 	    	cv.put("domain", domain);
 	    	db.insert(DB_DOMAINS_TABLE, null, cv);
     	}
+    	existingEntries.close();
     }
-    
+
     static void clearRememberedDomains(SQLiteDatabase db){
     	db.delete(DB_DOMAINS_TABLE, null, null);
     }
-    
-    
+
+
     /**
      * Creates an Adapter that looks for the start of a domain string from the database.
      * For use with the AutoCompleteTextView
-     * 
+     *
      * @param db the domain database
      * @return an Adapter that uses the Simple Dropdown Item view
      */
-    public SimpleCursorAdapter getDomainPrefixAdapter(SQLiteDatabase db){
+    public SimpleCursorAdapter getDomainPrefixAdapter(final Activity activity, SQLiteDatabase db){
 		final Cursor dbCursor = db.query(DB_DOMAINS_TABLE, null, null, null, null, null, "domain ASC");
-		
+		activity.startManagingCursor(dbCursor);
+
 		// Simple it says - ha!
-		final SimpleCursorAdapter adapter = new SimpleCursorAdapter(context, 
-				android.R.layout.simple_dropdown_item_1line, 
-				dbCursor, 
-				new String[] {"domain"}, 
+		final SimpleCursorAdapter adapter = new SimpleCursorAdapter(context,
+				android.R.layout.simple_dropdown_item_1line,
+				dbCursor,
+				new String[] {"domain"},
 				// Not sure if the below line is correct, but it seems to work.
 				new int[] {android.R.id.text1} );
-		
+
 		adapter.setStringConversionColumn(dbCursor.getColumnIndex("domain"));
-		
+
 		final SQLiteDatabase dbInner = db;
 		// a filter that searches for domains starting with the given constraint
 		adapter.setFilterQueryProvider(new FilterQueryProvider(){
 			public Cursor runQuery(CharSequence constraint) {
+				Cursor c;
 				if (constraint == null || constraint.length() == 0){
-					return dbInner.query(DB_DOMAINS_TABLE, null, null, null, null, null, "domain ASC");
+					c = dbInner.query(DB_DOMAINS_TABLE, null, null, null, null, null, "domain ASC");
 				}else{
-					return dbInner.query(DB_DOMAINS_TABLE, 
-							null, 
-							"domain GLOB ?", 
+					c = dbInner.query(DB_DOMAINS_TABLE,
+							null,
+							"domain GLOB ?",
 							new String[] {constraint.toString()+"*"}, null, null, "domain ASC");
 				}
+				activity.startManagingCursor(c);
+				return c;
 			}
 		});
 		return adapter;
     }
-	
+
 }
--- src/info/staticfree/SuperGenPass/Super_Gen_Pass.java
+++ src/info/staticfree/SuperGenPass/Super_Gen_Pass.java
@@ -67,41 +67,41 @@ public class Super_Gen_Pass extends Activity implements OnClickListener, OnLongC
 	private ArrayList<String> domains;
 	private static final int ABOUT_DIALOG = 0;
 	private static final int REQUEST_CODE_PREFERENCES = 0;
-	
+
 	private int pwLength;
 	private String pwType;
 	private String pwSalt;
 	private boolean copyToClipboard;
 	private boolean rememberDomains;
 	private boolean noDomainCheck;
-	
+
 	private GeneratedPasswordView genPwView;
 	private EditText domainEdit;
-	
+
 	private RememberedDBHelper dbHelper;
 	private SQLiteDatabase db;
-	
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
 		final Intent intent = getIntent();
 		final Uri data = intent.getData();
-		
-		// Make window transient-looking if coming from a share intent 
+
+		// Make window transient-looking if coming from a share intent
 		if (Intent.ACTION_SEND.equals(intent.getAction())){
 			setTheme(android.R.style.Theme_Dialog);
 		}else{
 			// this is necessary as the default theme is translucent to make the dimming work
 			setTheme(android.R.style.Theme);
 		}
-			
+
 		super.onCreate(savedInstanceState);
-		
+
         setContentView(R.layout.main);
-        
-        
+
+
         domainEdit = (EditText)findViewById(R.id.domain_edit);
-        
+
         genPwView = (GeneratedPasswordView) findViewById(R.id.password_output);
         genPwView.setOnLongClickListener(this);
         final EditText masterPwEdit = ((EditText)findViewById(R.id.password_edit));
@@ -112,34 +112,34 @@ public class Super_Gen_Pass extends Activity implements OnClickListener, OnLongC
 				return false;
 			}
 		});
-                
+
         updatePreferences();
-        
+
 		dbHelper = new RememberedDBHelper(getApplicationContext());
 		db = dbHelper.getWritableDatabase();
-        
+
 		loadDomains();
-        
+
         try {
 			md5 = MessageDigest.getInstance("MD5");
 		} catch (final NoSuchAlgorithmException e) {
 			e.printStackTrace();
-			Toast.makeText(getApplicationContext(), 
-					String.format(getString(R.string.err_no_md5), e.getLocalizedMessage()), 
+			Toast.makeText(getApplicationContext(),
+					String.format(getString(R.string.err_no_md5), e.getLocalizedMessage()),
 					Toast.LENGTH_LONG).show();
 		}
-		
+
 		// set up go button
 		((Button)findViewById(R.id.go)).setOnClickListener(this);
-		
+
 		// initialize the autocompletion
 		((AutoCompleteTextView)findViewById(R.id.domain_edit))
-			.setAdapter(dbHelper.getDomainPrefixAdapter(db));
-		
+			.setAdapter(dbHelper.getDomainPrefixAdapter(this, db));
+
 		// check for the "share page" intent. If present, pre-fill.
 
 		if (data == null){
-			
+
 			final String maybeUrl = intent.getStringExtra(Intent.EXTRA_TEXT);
 			if (maybeUrl != null){
 				try{
@@ -147,9 +147,9 @@ public class Super_Gen_Pass extends Activity implements OnClickListener, OnLongC
 					final Uri uri = Uri.parse(maybeUrl);
 					domainEdit.setText(getDomain(uri.getHost()));
 					((EditText)findViewById(R.id.password_edit)).requestFocus();
-					
+
 				}catch(final Exception e){
-					// nothing much to be done here. 
+					// nothing much to be done here.
 					// Let the user figure it out.
 					Log.e(TAG, "Could not find valid URI in shared text", e);
 					Toast.makeText(this, e.getLocalizedMessage(), Toast.LENGTH_LONG).show();
@@ -157,16 +157,16 @@ public class Super_Gen_Pass extends Activity implements OnClickListener, OnLongC
 			}
 		}
     }
-    
+
     @Override
     protected void onDestroy() {
     	db.close();
     	super.onDestroy();
     }
-    
+
     /**
      * This list should remain the same and in sync with the canonical SGP,
-	 * so that passwords generated in one place are the same as others.  
+	 * so that passwords generated in one place are the same as others.
      */
     private void loadDomains(){
     	final InputStream is = getResources().openRawResource(R.raw.domains);
@@ -185,30 +185,30 @@ public class Super_Gen_Pass extends Activity implements OnClickListener, OnLongC
     			domains.add(domainJson.getString(i));
     		}
     	}catch (final Exception e){
-    		Toast.makeText(this, getString(R.string.err_json_load, e.getLocalizedMessage()), 
+    		Toast.makeText(this, getString(R.string.err_json_load, e.getLocalizedMessage()),
     				Toast.LENGTH_LONG).show();
     		Log.d(TAG, getString(R.string.err_json_load), e);
     		finish();
     	}
-    	
+
     	Assert.assertTrue("Domains did not seem to load", domains.size() > 100);
     }
 
-    
+
     /**
      * Go!
      */
     void go(){
     	String genPw = "";
-    	final String domain = getDomain(); 
+    	final String domain = getDomain();
     	try {
         	if (pwType.equals("sgp")){
-        		genPw = superGenPassGen(getMasterPassword() + pwSalt, domain, pwLength);	
-        		
+        		genPw = superGenPassGen(getMasterPassword() + pwSalt, domain, pwLength);
+
         	}else if (pwType.equals("pwc")){
         		genPw = passwordComposerGen(getMasterPassword() + pwSalt, domain, pwLength);
         	}
-			
+
 		} catch (final PasswordGenerationException e) {
 			genPw = "";
 			Toast.makeText(this, e.getLocalizedMessage(), Toast.LENGTH_LONG).show();
@@ -216,20 +216,20 @@ public class Super_Gen_Pass extends Activity implements OnClickListener, OnLongC
 
 		genPwView.setDomainName(domain);
 		genPwView.setText(genPw);
-		
+
 		if (rememberDomains){
 			RememberedDBHelper.addRememberedDomain(db, domain);
 		}
-		
+
 		if (copyToClipboard){
 			genPwView.copyToClipboard();
-			
+
 			if (Intent.ACTION_SEND.equals(getIntent().getAction())){
 				finish();
 			}
 		}
     }
-    
+
     /**
      * @return the domain entered into the text box
      */
@@ -237,20 +237,20 @@ public class Super_Gen_Pass extends Activity implements OnClickListener, OnLongC
     	final AutoCompleteTextView txt = (AutoCompleteTextView)findViewById(R.id.domain_edit);
     	return txt.getText().toString();
     }
-    
+
     String getMasterPassword(){
     	final EditText txt = (EditText)findViewById(R.id.password_edit);
     	return txt.getText().toString();
-    } 
+    }
 
-    
+
     /**
      * Generates a domain password based on the PasswordComposer algorithm.
-     * 
+     *
      * @param masterPass master password
      * @param domain pre-filtered domain (eg. example.org)
      * @return generated password
-     * @throws PasswordGenerationException 
+     * @throws PasswordGenerationException
      * @see http://www.xs4all.nl/~jlpoutre/BoT/Javascript/PasswordComposer/
      */
     public String passwordComposerGen(String masterPass, String domain, int length) throws PasswordGenerationException{
@@ -259,15 +259,15 @@ public class Super_Gen_Pass extends Activity implements OnClickListener, OnLongC
     	}
     	return md5hex(new String(masterPass + ":" + getDomain(domain)).getBytes()).substring(0, length);
     }
-    
+
     /**
      * Generates a domain password based on the SuperGenPass algorithm.
-     * 
+     *
      * @param masterPass
      * @param domain pre-filtered domain (eg. example.org)
      * @param length generated password length; an integer between 4 and 24, inclusive.
      * @return generated password
-     * @throws PasswordGenerationException 
+     * @throws PasswordGenerationException
      * @see http://supergenpass.com/
      */
     public String superGenPassGen(String masterPass, String domain, int length) throws PasswordGenerationException{
@@ -277,14 +277,14 @@ public class Super_Gen_Pass extends Activity implements OnClickListener, OnLongC
     	if (domain.equals("")){
     		throw new PasswordGenerationException("Missing domain");
     	}
-    	
+
     	 String pwSeed = masterPass + ":" + getDomain(domain);
- 
+
     	// wash ten times
     	for (int i = 0; i < 10; i++){
     		pwSeed = md5base64(pwSeed.getBytes());
     	}
-    	    	
+
     	/*   from http://supergenpass.com/about/#PasswordComplexity :
     	        *  Consist of alphanumerics (A-Z, a-z, 0-9)
 			    * Always start with a lowercase letter of the alphabet
@@ -292,25 +292,25 @@ public class Super_Gen_Pass extends Activity implements OnClickListener, OnLongC
 			    * Always contain at least one numeral
 			    * Can be any length from 4 to 24 characters (default: 10)
     	 */
-   
+
     	// regex looks for:
-    	// "lcletter stuff Uppercase stuff Number stuff" or 
+    	// "lcletter stuff Uppercase stuff Number stuff" or
     	// "lcletter stuff Number stuff Uppercase stuff"
     	// which should satisfy the above requirements.
     	while (! pwSeed.substring(0,length).
     			matches("^[a-z][a-zA-Z0-9]*(?:(?:[A-Z][a-zA-Z0-9]*[0-9])|(?:[0-9][a-zA-Z0-9]*[A-Z]))[a-zA-Z0-9]*$")){
     		pwSeed = md5base64(pwSeed.getBytes());
     	}
-    	
-    	// when the right pwSeed is found to have a 
+
+    	// when the right pwSeed is found to have a
     	// password-appropriate substring, return it
     	return pwSeed.substring(0, length);
     }
-    
- 
+
+
     /**
      * Returns the standard hex-encoded string md5sum of the data.
-     * 
+     *
      * @param data
      * @return hex-encoded string of the md5sum of the data
      */
@@ -318,59 +318,59 @@ public class Super_Gen_Pass extends Activity implements OnClickListener, OnLongC
     	final byte[] md5data = md5.digest(data);
     	String md5hex = new String();
     	for( int i = 0; i < md5data.length; i++){
-    		md5hex += String.format("%02x", md5data[i]); 
+    		md5hex += String.format("%02x", md5data[i]);
     	}
     	return md5hex;
     }
-    
+
     /**
      * Returns a base64-encoded string of the md5sum of the data.
      * Caution: SuperGenPass-specific!
      * Includes substitutions to ensure that valid base64 characters
      * '=', '/', and '+' get mapped to
      * 'A', '8', and '9' respectively, so as to ensure alpha/num passwords.
-     *  
+     *
      * @param data
      * @return  base64-encoded string of the md5sum of the data
      */
     public String md5base64(byte[] data){
-    	
+
     	String b64 = new String(Base64.encodeBase64(md5.digest(data)));
     	// SuperGenPass-specific quirk so that these don't end up in the password.
     	b64 = b64.replace('=', 'A').replace('/', '8').replace('+', '9');
     	b64.trim();
-    	
+
     	return b64;
     }
-    
+
     /**
      * Computes the site's domain, based on the provided hostname. This takes into account
      * things like "co.uk" and other such multi-level TLDs.
-     * 
+     *
      * @param hostname the full hostname
      * @return the domain of the URI
-     * @throws PasswordGenerationException 
+     * @throws PasswordGenerationException
      */
     public String getDomain(String hostname) throws PasswordGenerationException{
-    	
+
     	hostname = hostname.toLowerCase();
 
     	if (noDomainCheck){
     		return hostname;
     	}
-    	    	
+
     	// IP addresses should be composed based on the full address.
     	if (hostname.matches("^\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}$")){
     		return hostname;
     	}
-    	
+
     	// for single-level TLDs, we only want the TLD and the 2nd level domain
     	final String[] hostParts = hostname.split("\\.");
     	if (hostParts.length < 2){
     		throw new PasswordGenerationException("Invalid domain: '"+hostname+"'");
     	}
     	String domain = hostParts[hostParts.length-2] + '.' + hostParts[hostParts.length-1];
-    	
+
     	// do a slow search of all the possible multi-level TLDs and
     	// see if we need to pull in one level deeper.
     	for (final String tld: domains){
@@ -384,7 +384,7 @@ public class Super_Gen_Pass extends Activity implements OnClickListener, OnLongC
     	}
     	return domain;
     }
-    
+
 	public void onClick(View v) {
 		switch (v.getId()){
 		case R.id.go:
@@ -393,7 +393,7 @@ public class Super_Gen_Pass extends Activity implements OnClickListener, OnLongC
 		case R.id.password_output:
 			break;
 		}
-		
+
 	}
 
 	public boolean onLongClick(View v) {
@@ -401,13 +401,13 @@ public class Super_Gen_Pass extends Activity implements OnClickListener, OnLongC
 		}
 		return false;
 	}
-	
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
     	getMenuInflater().inflate(R.menu.options, menu);
     	return true;
     }
-    
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
     	switch (item.getItemId()){
@@ -417,36 +417,36 @@ public class Super_Gen_Pass extends Activity implements OnClickListener, OnLongC
             startActivityForResult(preferencesIntent, REQUEST_CODE_PREFERENCES);
 
     		return true;
-    		
+
     	case R.id.about:
     		showDialog(ABOUT_DIALOG);
     		return true;
     	}
     	return false;
     }
-    
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
     	super.onActivityResult(requestCode, resultCode, data);
-    	
+
     	if (requestCode == REQUEST_CODE_PREFERENCES){
     		updatePreferences();
     	}
     }
-    
+
     @Override
     protected Dialog onCreateDialog(int id) {
     	switch (id){
     	case ABOUT_DIALOG:
         	final Builder builder = new AlertDialog.Builder(this);
-        	
+
         	builder.setTitle(R.string.about_title);
         	builder.setIcon(R.drawable.icon);
-        	
+
         	// using this instead of setMessage lets us have clickable links.
         	final LayoutInflater factory = LayoutInflater.from(this);
         	builder.setView(factory.inflate(R.layout.about, null));
-        	
+
         	builder.setPositiveButton("Ok", new DialogInterface.OnClickListener(){
         		public void onClick(DialogInterface dialog, int which) {
         			setResult(RESULT_OK);
@@ -456,10 +456,10 @@ public class Super_Gen_Pass extends Activity implements OnClickListener, OnLongC
     	}
         return null;
     }
-    
+
     protected void  updatePreferences(){
     	final SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
-    	
+
     	// when adding items here, make sure default values are in sync with the xml file
     	this.pwType = prefs.getString("pw_type", "sgp");
     	this.pwLength = Integer.parseInt(prefs.getString("pw_length", "10"));
@@ -468,13 +468,13 @@ public class Super_Gen_Pass extends Activity implements OnClickListener, OnLongC
     	this.rememberDomains = prefs.getBoolean("domain_autocomplete", true);
     	this.noDomainCheck = prefs.getBoolean("domain_nocheck", false);
 
-    	
+
     	// While it doesn't really make sense to clear this every time this is saved,
     	// there isn't much of a better option beyond remembering more state.
     	if (! rememberDomains){
     		RememberedDBHelper.clearRememberedDomains(db);
     	}
-    	
+
     	if (noDomainCheck){
     		domainEdit.setHint(R.string.domain_hint_no_checking);
         }else{
