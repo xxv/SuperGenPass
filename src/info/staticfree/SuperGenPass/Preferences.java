f3d387592badc4d35ccd5746af2c7b01bb058b8d
Steve Pomeroy <steve@staticfree.info>
2012-08-18 05:11:48 -0400

    Adds ability to generate salt
    The salt, once generated, is displayed as a QR code.

--- src/info/staticfree/SuperGenPass/Preferences.java
+++ src/info/staticfree/SuperGenPass/Preferences.java
@@ -1,5 +1,12 @@
 package info.staticfree.SuperGenPass;
 
+import java.security.SecureRandom;
+
+import org.apache.commons.codec.binary.Base64;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -16,6 +23,7 @@ import com.google.zxing.integration.android.IntentResult;
 public class Preferences extends PreferenceActivity {
 
 	public static final String SCAN_SALT = "info.staticfree.android.supergenpass.SCAN_SALT";
+	public static final String GENERATE_SALT = "info.staticfree.android.supergenpass.GENERATE_SALT";
 
 	// @formatter:off
 	public static final String
@@ -70,8 +78,48 @@ public class Preferences extends PreferenceActivity {
 	@Override
 	protected void onNewIntent(Intent intent) {
 
-		if (SCAN_SALT.equals(intent.getAction())) {
+		final String action = intent.getAction();
+
+		if (SCAN_SALT.equals(action)) {
 			scanSalt();
+
+		} else if (GENERATE_SALT.equals(action)) {
+			showDialog(DIALOG_GENERATE);
+		}
+	}
+
+	private final int DIALOG_GENERATE = 100;
+
+	@Override
+	protected Dialog onCreateDialog(int id) {
+		switch (id) {
+			case DIALOG_GENERATE:
+
+				return new AlertDialog.Builder(this)
+						.setTitle(R.string.pref_generate_salt_title)
+						.setMessage(
+								R.string.pref_generate_salt_dialog_message)
+						.setPositiveButton(R.string.pref_generate_salt_and_set,
+								new DialogInterface.OnClickListener() {
+
+									@Override
+									public void onClick(DialogInterface dialog, int which) {
+										generateSalt();
+
+									}
+								})
+						.setCancelable(true)
+						.setNegativeButton(android.R.string.cancel,
+								new DialogInterface.OnClickListener() {
+
+									@Override
+									public void onClick(DialogInterface dialog, int which) {
+										dialog.dismiss();
+
+									}
+								}).create();
+			default:
+				return super.onCreateDialog(id);
 		}
 	}
 
@@ -83,6 +131,21 @@ public class Preferences extends PreferenceActivity {
 		qr.initiateScan(IntentIntegrator.QR_CODE_TYPES);
 	}
 
+	private void generateSalt() {
+		final IntentIntegrator qr = new IntentIntegrator(this);
+		final SecureRandom sr = new SecureRandom();
+		final byte[] salt = new byte[512];
+		sr.nextBytes(salt);
+		final String saltb64 = new String(Base64.encodeBase64(salt)).replaceAll("\\s", "");
+		setSaltPref(saltb64);
+		qr.addExtra(Intents.Encode.SHOW_CONTENTS, false);
+		qr.shareText(saltb64);
+	}
+
+	private void setSaltPref(String salt) {
+		((EditTextPreference) findPreference(PREF_PW_SALT)).setText(salt);
+	}
+
 	@SuppressWarnings("deprecation")
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -91,7 +154,7 @@ public class Preferences extends PreferenceActivity {
 
 		if (res != null && res.getContents() != null) {
 			final String salt = res.getContents();
-			((EditTextPreference) findPreference(PREF_PW_SALT)).setText(salt);
+			setSaltPref(salt);
 		}
 	}
 
